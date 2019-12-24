7fcb2cdc97f2223145c5d78c7617ca88807f7574
moonpeter <moonpeter@kakao.com>
2019-12-24 18:38:25 +0900

    login_view기능구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,6 +1,20 @@
-# Create your views here.
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
+
+# Create your views here.
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print('username', username)
+        print('password', password)
+        user = authenticate(request, username=username, password=password)
+        print('user:', user)
+        if user:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('members:login')
     return render(request, 'members/login.html')
